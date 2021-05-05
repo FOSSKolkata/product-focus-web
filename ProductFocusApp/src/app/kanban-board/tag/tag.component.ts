a0c6aa1e72ff5bd79f2ed1de2944c8dc59ca1569
yes-v <mevikramshaw@gmail.com>
2021-05-05 20:54:25 +0530

    tagging mockup added

--- ProductFocusApp/src/app/kanban-board/tag/tag.component.ts
+++ ProductFocusApp/src/app/kanban-board/tag/tag.component.ts
@@ -1,60 +1,65 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { merge, Observable, OperatorFunction, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { isTemplateExpression } from 'typescript';
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
   styleUrls: ['./tag.component.css']
 })
-export class TagComponent implements OnInit {
 
+export class TagComponent implements OnInit {
+  @ViewChild('instance', { static: true }) instance!: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+  tagName: any;
+  taglist: Array<string> = [];
+  isAddTagActive:boolean = false;
+  
+  tagsData = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  
   constructor() { }
-  @ViewChild('auto') auto: any;
-  focus(e:any): void {
-    setTimeout(()=>{
-      this.auto.focus();
-    },0);
-  }
   
   ngOnInit(): void {
-  }
-  keyword = 'name';
-  data = [
-     {
-       id: 1,
-       name: 'PNR medication needs to transfer to a new tab'
-     },
-     {
-       id: 2,
-       name: 'Login button is not aligned properly.'
-     },
-     {
-       id: 3,
-       name: 'Title in progress 1'
-     }
-  ];
-  tagName: string = '';
-  taglist: Array<{id:number,name:string}> = [];
-  isAddTagActive:boolean = false;
-  addTag(item: any){
-    this.taglist.push({id:this.taglist.length,name:item.name});
-    this.isAddTagActive = false;
-    var index = this.data.indexOf(item);
-    this.data.splice(index,1);
-    setTimeout(()=>this.tagName = '',0);
+
   }
 
-  removeTag(tagDetails: {id: number, name: string}){
-    var index = this.taglist.indexOf(tagDetails);
-    this.data.push(tagDetails);
+  inputTextFocus(inputText:any): void {
+    setTimeout(()=> inputText.focus(),0);
+  }
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(debounceTime(200), distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.tagsData
+        : this.tagsData.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    );
+  }
+
+  addTag(event: any){
+    setTimeout(()=>this.tagName = '',0);
+    var index = this.taglist.indexOf(event.item);
+    if(index != -1) {
+      console.log(event.item + " is already added");
+      return;
+    }
+    this.taglist.push(event.item);
+  }
+
+  removeTag(name: string){
+    var index = this.taglist.indexOf(name);
     this.taglist.splice(index,1);
   }
-
-  deactiveAddTag(){
-    setTimeout(()=>this.isAddTagActive=false,0);
-  }
-
-  activeAddTag(){
-    setTimeout(()=>this.isAddTagActive=true,0);
-  }
-
 }
